a7ee579ce64c8b7d52f926046371edd1e1ca125c
Ankit Raj <ankitraj@usf.edu>
2020-01-21 21:18:25 -0500

    Changes

--- Exercise2.py
+++ Exercise2.py
@@ -5,7 +5,7 @@ class Exercise2:
 
         #Max heartrate is equal to 220 - age
         #Console output
-        print("I am ", age, " years old, my heart rate is ", 220 - age, " and I am ", height, " inches tall.")
+        print("I am ", age, " years old, my heart rate is ", 220 - age, " and I am ", height, " inches tall")
 
     if __name__ == "__main__":
         main()
