ad4c25294caa271457c22e68911100aa8f02528e
Ankit Raj <ankitraj@usf.edu>
2020-01-21 21:16:24 -0500

    Add project files.

---
+++ Exercise2.py
@@ -0,0 +1,11 @@
+class Exercise2:
+    def main():
+        age = 26
+        height = 180
+
+        #Max heartrate is equal to 220 - age
+        #Console output
+        print("I am ", age, " years old, my heart rate is ", 220 - age, " and I am ", height, " inches tall.")
+
+    if __name__ == "__main__":
+        main()
